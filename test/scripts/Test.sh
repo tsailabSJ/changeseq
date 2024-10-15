72bac2a90ff81d3c4d1dc547bdae247c1e406654
YichaoOU <yli11@splprhpc11.cm.cluster>
2024-10-15 11:18:34 -0500

    update test example

--- test/scripts/Test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python ../../circleseq/circleseq.py all --manifest ../CIRCLEseq_MergedTest.yaml
-
-python ../../circleseq/circleseq.py all --manifest ../CIRCLEseq_StandardTest.yaml
